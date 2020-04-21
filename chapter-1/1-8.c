aa85414adf3ff94c9c7b0c3874ee419d5cb0b240
L1nker4 <927022262@qq.com>
2020-04-21 08:54:21 +0000

    demo

--- chapter-1/1-8.c
+++ chapter-1/1-8.c
@@ -1,10 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
     int c;
-    while ((c = getchar()) != EOF){
-        putchar(c);
+    int blank = 0, tab = 0, nextline = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            blank++;
+        } else if (c == '\t') {
+            tab++;
+        } else if (c == '\n') {
+            nextline++;
+        }
     }
+    printf("\nblank  = %d, tab = %d, nextline = %d \n", blank, tab, nextline);
     return 0;
-    
 }
